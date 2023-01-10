6c9b0b8fcb3ccb0c02ac90c411c08ea83259a732
inmacc96 <inmacaballerocarrero@gmail.com>
2023-01-10 10:50:29 +0100

    Add Types and Resolvers

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,49 @@
 const { ApolloServer, gql } = require("apollo-server");
 
+// Schema
+// Con gql indica que es código de graphQL
+const typeDefs = gql`
+  type Course {
+    title: String
+    technology: String
+  }
+
+  type Query {
+    getCourses: [Course]
+  }
+`;
+
+const courses = [
+  {
+      title: 'JavaScript Moderno Guía Definitiva Construye +10 Proyectos',
+      technology: 'JavaScript ES6',
+  },
+  {
+      title: 'React – La Guía Completa: Hooks Context Redux MERN +15 Apps',
+      technology: 'React',
+  },
+  {
+      title: 'Node.js – Bootcamp Desarrollo Web inc. MVC y REST API’s',
+      technology: 'Node.js'
+  }, 
+  {
+      title: 'ReactJS Avanzado – FullStack React GraphQL y Apollo',
+      technology: 'React'
+  }
+];
+
+// Resolvers
+const resolvers = {
+  Query: {
+    getCourses: () => {
+      return courses
+    },
+  },
+};
+
+
 // Servidor
-const server = new ApolloServer();
+const server = new ApolloServer({typeDefs, resolvers});
 
 // Arrancar el servidor
 server.listen().then(({ url }) => {
