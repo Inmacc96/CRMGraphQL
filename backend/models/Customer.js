d54f8737c4a91a39610118a4d815b3397ea4d3d3
inmacc96 <inmacaballerocarrero@gmail.com>
2023-01-11 10:42:43 +0100

    Define Customer model

---
+++ backend/models/Customer.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+const CustomerSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  surname: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  company: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+  },
+  phone: {
+    type: String,
+    trim: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  seller: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "User",
+  },
+});
+
+module.exports = mongoose.Model("Customer", CustomerSchema);
