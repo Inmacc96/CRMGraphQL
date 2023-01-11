96c87353a2d974c48508b39c6b8b66d81063e991
inmacc96 <inmacaballerocarrero@gmail.com>
2023-01-11 12:21:20 +0100

    Create Order model

---
+++ backend/models/Order.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const OrderSchema = mongoose.Schema({
+  order: {
+    type: Array,
+    required: true,
+  },
+  total: {
+    type: Number,
+    required: true,
+  },
+  customer: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: Customer,
+  },
+  seller: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: User,
+  },
+  state: {
+    type: String,
+    default: "PENDING",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+module.exports = mongoose.model("Order", OrderSchema);
