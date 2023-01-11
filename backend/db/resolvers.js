505c8f5edf6a943b1e2543c5390990d10041efd5
inmacc96 <inmacaballerocarrero@gmail.com>
2023-01-11 09:45:51 +0100

    Create product model

--- backend/db/resolvers.js
+++ backend/db/resolvers.js
@@ -1,4 +1,5 @@
 const User = require("../models/User");
+const Product = require("../models/Product");
 const bcryptjs = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 require("dotenv").config({ path: ".env" });
