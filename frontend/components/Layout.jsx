4a3bd989ab0eadba70c8041e90c80ffd33cf5345
inmacc96 <inmacaballerocarrero@gmail.com>
2023-01-13 19:28:00 +0100

    Define layout for login and signup pages

--- frontend/components/Layout.jsx
+++ frontend/components/Layout.jsx
@@ -1,20 +1,30 @@
 import Head from "next/head";
 import Sidebar from "./Sidebar";
+import { useRouter } from "next/router";
 
 const Layout = ({ children }) => {
+  // Hook de routing
+  const router = useRouter();
+
   return (
     <>
       <Head>
         <title>CRM - Customer Management</title>
       </Head>
 
-      <div className="bg-gray-200 min-h-screen sm:flex">
-        <Sidebar />
-
-        <main className="sm:w-2/3 xl:w-4/5 sm:min-h-screen p-5">
+      {["/login", "/signup"].includes(router.pathname) ? (
+        <div className="bg-gray-800 min-h-screen flex flex-col justify-center">
           {children}
-        </main>
-      </div>
+        </div>
+      ) : (
+        <div className="bg-gray-200 min-h-screen sm:flex">
+          <Sidebar />
+
+          <main className="sm:w-2/3 xl:w-4/5 sm:min-h-screen p-5">
+            {children}
+          </main>
+        </div>
+      )}
     </>
   );
 };
