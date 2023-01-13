97710aea39a9cc1384379f0125c1e6e229a63a59
inmacc96 <inmacaballerocarrero@gmail.com>
2023-01-13 18:19:28 +0100

    Create CRMGraphQL frontend application with Nextjs

---
+++ frontend/components/Layout.jsx
@@ -0,0 +1,10 @@
+const Layout = ({ children }) => {
+  return (
+    <>
+      <h1>Desde Layout</h1>
+      {children}
+    </>
+  );
+};
+
+export default Layout;
---
+++ frontend/pages/index.js
@@ -0,0 +1,9 @@
+import Layout from "../components/Layout";
+
+export default function Home() {
+  return (
+    <Layout>
+      <h2>Desde Index</h2>
+    </Layout>
+  );
+}
