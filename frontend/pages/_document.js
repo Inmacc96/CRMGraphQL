1be94be7d5dba74d0b215c5d3d1dba031df37b5f
inmacc96 <inmacaballerocarrero@gmail.com>
2023-01-13 18:36:25 +0100

    Add normalize css and a description

---
+++ frontend/pages/_document.js
@@ -0,0 +1,24 @@
+import { Html, Head, Main, NextScript } from "next/document";
+const Document = () => {
+  return (
+    <Html>
+      <Head>
+        <meta name="description" content="CRM - Customer Management" />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
+          integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w=="
+          crossOrigin="anonymous"
+          referrerpolicy="no-referrer"
+        />
+      </Head>
+
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+};
+
+export default Document;
