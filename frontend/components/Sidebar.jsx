34d38fee3b1a79964e10226379624828ee3727a2
inmacc96 <inmacaballerocarrero@gmail.com>
2023-01-13 19:13:48 +0100

    Highlight current page

--- frontend/components/Sidebar.jsx
+++ frontend/components/Sidebar.jsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 const Sidebar = () => {
+  const router = useRouter();
   return (
     <aside className="bg-gray-800 sm:w-1/3 xl:w-1/5 sm:min-h-screen p-5">
       <div>
@@ -8,14 +10,25 @@ const Sidebar = () => {
       </div>
 
       <nav className="mt-5 list-none">
-        <li>
-          <Link href="/" className="text-white mb-2 block">
+        <li className={router.pathname === "/" ? "bg-blue-800 p-2" : "p-2"}>
+          <Link href="/" className="text-white block">
             Customers
           </Link>
         </li>
-        <li>
-          <Link href="/orders" className="text-white mb-2 block">
+        <li
+          className={router.pathname === "/orders" ? "bg-blue-800 p-2" : "p-2"}
+        >
+          <Link href="/orders" className="text-white block">
             Orders
+          </Link>
+        </li>
+        <li
+          className={
+            router.pathname === "/products" ? "bg-blue-800 p-2" : "p-2"
+          }
+        >
+          <Link href="/products" className="text-white block">
+            Products
           </Link>
         </li>
       </nav>
