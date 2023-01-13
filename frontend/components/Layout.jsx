7308cb1a6e40e3884bb7c979333ba60569e9a655
inmacc96 <inmacaballerocarrero@gmail.com>
2023-01-13 18:36:53 +0100

    Add a title to the app

--- frontend/components/Layout.jsx
+++ frontend/components/Layout.jsx
@@ -1,6 +1,11 @@
+import Head from "next/head";
+
 const Layout = ({ children }) => {
   return (
     <>
+      <Head>
+        <title>CRM - Customer Management</title>
+      </Head>
       <h1>Desde Layout</h1>
       {children}
     </>
