6ca838579f6961af8656cf1f22bbe359a8da4a4a
inmacc96 <inmacaballerocarrero@gmail.com>
2023-01-16 10:00:42 +0100

    Install and config Apollo

---
+++ frontend/config/apollo.js
@@ -0,0 +1,12 @@
+import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
+import fetch from "node-fetch";
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: "http://localhost:4000/",
+    fetch,
+  }),
+});
+
+export default client;
