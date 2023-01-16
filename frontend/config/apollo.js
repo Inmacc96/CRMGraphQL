eb7acba2d9daf2ed9a3d869387b197986ff34b21
inmacc96 <inmacaballerocarrero@gmail.com>
2023-01-16 10:13:39 +0100

    Add apollo to app and install graphql

--- frontend/config/apollo.js
+++ frontend/config/apollo.js
@@ -1,11 +1,9 @@
 import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
-import fetch from "node-fetch";
 
 const client = new ApolloClient({
   cache: new InMemoryCache(),
   link: new HttpLink({
     uri: "http://localhost:4000/",
-    fetch,
   }),
 });
 
