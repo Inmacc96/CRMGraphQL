8e08c7e4377a60a0b170851a36b2ab08732e1b9e
inmacc96 <inmacaballerocarrero@gmail.com>
2023-01-18 16:54:11 +0100

    Design the form for a new client

--- frontend/pages/newCustomer.js
+++ frontend/pages/newCustomer.js
@@ -4,6 +4,93 @@ const newCustomer = () => {
   return (
     <Layout>
       <h1 className="text-2xl text-gray-800 font-light"> New Customer</h1>
+
+      <div className="flex justify-center mt-5">
+        <div className="w-full max-w-lg">
+          <form className="bg-white shadow-md px-8 pt-6 pb-8 mb-4">
+            <div className="mb-4">
+              <label
+                htmlFor="name"
+                className="block text-gray-700 text-sm font-bold mb-2"
+              >
+                Name
+              </label>
+              <input
+                id="name"
+                type="text"
+                placeholder="Customer Name"
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-gray-800"
+              />
+            </div>
+
+            <div className="mb-4">
+              <label
+                htmlFor="surname"
+                className="block text-gray-700 text-sm font-bold mb-2"
+              >
+                Surname
+              </label>
+              <input
+                id="surname"
+                type="text"
+                placeholder="Customer Surname"
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-gray-800"
+              />
+            </div>
+
+            <div className="mb-4">
+              <label
+                htmlFor="company"
+                className="block text-gray-700 text-sm font-bold mb-2"
+              >
+                Company
+              </label>
+              <input
+                id="company"
+                type="text"
+                placeholder="Customer Company"
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-gray-800"
+              />
+            </div>
+
+            <div className="mb-4">
+              <label
+                htmlFor="email"
+                className="block text-gray-700 text-sm font-bold mb-2"
+              >
+                Email
+              </label>
+              <input
+                id="email"
+                type="email"
+                placeholder="Customer Email"
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-gray-800"
+              />
+            </div>
+
+            <div className="mb-4">
+              <label
+                htmlFor="phone"
+                className="block text-gray-700 text-sm font-bold mb-2"
+              >
+                Phone
+              </label>
+              <input
+                id="phone"
+                type="tel"
+                placeholder="Customer Phone"
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-gray-800"
+              />
+            </div>
+
+            <input
+              type="submit"
+              className="bg-gray-800 w-full mt-5 p-2 text-white uppercase font-bold hover:bg-gray-900 hover:cursor-pointer"
+              value="register customer"
+            />
+          </form>
+        </div>
+      </div>
     </Layout>
   );
 };
