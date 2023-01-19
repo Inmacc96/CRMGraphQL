48de45b51148db0264edc2e1c7bed0383fea939f
inmacc96 <inmacaballerocarrero@gmail.com>
2023-01-19 17:01:09 +0100

    Get all products and display their information on the Products page.

--- frontend/pages/products.js
+++ frontend/pages/products.js
@@ -1,9 +1,45 @@
 import Layout from "../components/Layout";
+import Product from "../components/Product";
+import { useQuery, gql } from "@apollo/client";
+
+const GET_PRODUCTS = gql`
+  query getProducts {
+    getProducts {
+      id
+      name
+      price
+      stock
+    }
+  }
+`;
 
 const Products = () => {
+  // Obtener los productos
+  const { data, loading, error } = useQuery(GET_PRODUCTS);
+  console.log(data);
+
+  if (loading) return <p>Loading...</p>;
   return (
     <Layout>
       <h1 className="text-2xl text-gray-800 font-light">Products</h1>
+
+      <table className="table-auto shadow-md mt-10 w-full">
+        <thead className="bg-gray-800">
+          <tr className="text-white">
+            <th className="w-1/5 py-2">Name</th>
+            <th className="w-1/5 py-2">Stock</th>
+            <th className="w-1/5 py-2">Price</th>
+            <th className="w-1/5 py-2">Delete</th>
+            <th className="w-1/5 py-2">Edit</th>
+          </tr>
+        </thead>
+
+        <tbody className="bg-white">
+          {data.getProducts.map((product) => (
+            <Product key={product.id} product={product} />
+          ))}
+        </tbody>
+      </table>
     </Layout>
   );
 };
