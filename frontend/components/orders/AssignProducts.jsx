29b395680f471ba6b1fe5ab0f6f4477563276756
inmacc96 <inmacaballerocarrero@gmail.com>
2023-01-24 12:46:02 +0100

    Get the products from the db and display them in select

--- frontend/components/orders/AssignProducts.jsx
+++ frontend/components/orders/AssignProducts.jsx
@@ -1,6 +1,29 @@
 import Select from "react-select";
+import { gql, useQuery } from "@apollo/client";
+
+const GET_PRODUCTS = gql`
+  query getProducts {
+    getProducts {
+      id
+      name
+      price
+      stock
+    }
+  }
+`;
 
 const AssignProducts = () => {
+  // Obtener los productos de la base de datos
+  const { data, loading, error } = useQuery(GET_PRODUCTS);
+
+  if (loading) return null;
+
+  const { getProducts } = data;
+
+  const selectProducts = (values) => {
+    console.log(values);
+  };
+
   return (
     <>
       <p className="mt-10 my-2 bg-white border-l-4 border-gray-800 text-gray-700 p-3 text-sm font-bold">
@@ -10,11 +33,14 @@ const AssignProducts = () => {
         className="mt-3"
         id="products-select"
         instanceId="products-select"
-        /*    options={getCustomersSeller}
-        onChange={selectCustomer} */
+        isMulti={true}
+        options={getProducts}
+        onChange={selectProducts}
         getOptionValue={(option) => option.id}
-        getOptionLabel={(option) => option.name}
-        placeholder="Search or select the products"
+        getOptionLabel={(option) =>
+          `${option.name} - ${option.stock} available`
+        }
+        placeholder="Search or select the product"
       />
     </>
   );
