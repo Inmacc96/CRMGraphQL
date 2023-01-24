5bffd46cffcdeeffb8dfc2752189b3dbd747533f
inmacc96 <inmacaballerocarrero@gmail.com>
2023-01-24 19:13:23 +0100

    Save the new order in db

--- frontend/pages/newOrder.js
+++ frontend/pages/newOrder.js
@@ -5,10 +5,22 @@ import AssignProducts from "../components/orders/AssignProducts";
 import OrderSummary from "../components/orders/OrderSummary";
 import TotalPayable from "../components/orders/TotalPayable";
 import OrderContext from "../context/orders/OrderContext";
+import { useMutation, gql } from "@apollo/client";
+
+const NEW_ORDER = gql`
+  mutation newOrder($input: OrderInput) {
+    newOrder(input: $input) {
+      id
+    }
+  }
+`;
 
 const NewOrder = () => {
   // Context de pedidos
   const { customer, products, total } = useContext(OrderContext);
+
+  // Mutation para crear un nuevo pedido
+  const [newOrder] = useMutation(NEW_ORDER);
 
   const validateOrder = () => {
     return !products.every((product) => product.quantity > 0) ||
@@ -16,6 +28,27 @@ const NewOrder = () => {
       Object.keys(customer).length === 0
       ? " opacity-50  cursor-not-allowed "
       : "";
+  };
+
+  const createNewOrder = async () => {
+    try {
+      const { data } = await newOrder({
+        variables: {
+          input: {
+            customer: customer.id,
+            total,
+            order: products.map((product) => ({
+              id: product.id,
+              quantity: product.quantity,
+            })),
+          },
+        },
+      });
+
+      console.log(data);
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   return (
@@ -32,6 +65,7 @@ const NewOrder = () => {
           <button
             type="button"
             className={`bg-gray-800 w-full mt-5 p-2 text-white uppercase font-bold hover:bg-gray-900 ${validateOrder()}`}
+            onClick={createNewOrder}
           >
             Register Order
           </button>
