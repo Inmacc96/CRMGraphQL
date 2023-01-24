3ec47141fb0631d1d2df223fe476bae4a52f409d
inmacc96 <inmacaballerocarrero@gmail.com>
2023-01-24 12:20:09 +0100

    Get the clients from the db and display them in select

--- frontend/components/orders/AssignCustomer.jsx
+++ frontend/components/orders/AssignCustomer.jsx
@@ -1,14 +1,24 @@
 import { useState, useEffect } from "react";
 import Select from "react-select";
+import { useQuery, gql } from "@apollo/client";
 
-const customers = [
-  { id: 1, name: "Inma" },
-  { id: 2, name: "Pepe" },
-  { id: 3, name: "Maria" },
-];
+const GET_CUSTOMERS_USER = gql`
+  query getCustomersSeller {
+    getCustomersSeller {
+      id
+      name
+      surname
+      company
+      email
+    }
+  }
+`;
 
 const AssignCustomer = () => {
-  const [customer, setCustomer] = useState([]);
+  const [customer, setCustomer] = useState({});
+
+  // Consultar la base de datos para obtener los clientes
+  const { data, loading, error } = useQuery(GET_CUSTOMERS_USER);
 
   useEffect(() => {
     console.log(customer);
@@ -18,17 +28,27 @@ const AssignCustomer = () => {
     setCustomer(values);
   };
 
+  // Resultados de la consulta
+  if (loading) return null;
+
+  const { getCustomersSeller } = data;
+
   return (
-    <Select
-      id="long-value-select"
-      instanceId="long-value-select"
-      options={customers}
-      isMulti={true}
-      onChange={selectCustomer}
-      getOptionValue={(option) => option.id}
-      getOptionLabel={(option) => option.name}
-      placeholder="Select the customer"
-    />
+    <>
+      <p className="mt-10 my-2 bg-white border-l-4 border-gray-800 text-gray-700 p-3 text-sm font-bold">
+        1.- Assigns a customer to the order
+      </p>
+      <Select
+        className="mt-3"
+        id="long-value-select"
+        instanceId="long-value-select"
+        options={getCustomersSeller}
+        onChange={selectCustomer}
+        getOptionValue={(option) => option.id}
+        getOptionLabel={(option) => option.name}
+        placeholder="Search or select the customer"
+      />
+    </>
   );
 };
 
