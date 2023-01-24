025a88e6cdd487ec164692ae02073cf79b6517e0
inmacc96 <inmacaballerocarrero@gmail.com>
2023-01-24 19:18:40 +0100

    Display an alert message in case of unavailability of a selected product

--- frontend/pages/newOrder.js
+++ frontend/pages/newOrder.js
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import Layout from "../components/Layout";
 import AssignCustomer from "../components/orders/AssignCustomer";
 import AssignProducts from "../components/orders/AssignProducts";
@@ -16,6 +16,9 @@ const NEW_ORDER = gql`
 `;
 
 const NewOrder = () => {
+  // State de mensaje
+  const [msg, setMsg] = useState(null);
+
   // Context de pedidos
   const { customer, products, total } = useContext(OrderContext);
 
@@ -47,13 +50,23 @@ const NewOrder = () => {
 
       console.log(data);
     } catch (err) {
-      console.log(err);
+      setMsg(err.message.replace("Apollo error: ", ""));
+
+      setTimeout(() => {
+        setMsg(null);
+      }, 3000);
     }
   };
 
   return (
     <Layout>
       <h1 className="text-2xl text-gray-800 font-light">New Order</h1>
+
+      {msg && (
+        <div className="bg-white py-2 px-3 w-full my-3 max-w-sm text-center mx-auto">
+          <p>{msg}</p>
+        </div>
+      )}
 
       <div className="flex justify-center mt-5">
         <div className="w-full max-w-lg">
