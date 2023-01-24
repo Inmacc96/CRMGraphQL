580e5cde3c65c8719ca7ce50ce614307834f07c7
inmacc96 <inmacaballerocarrero@gmail.com>
2023-01-24 11:41:46 +0100

    Design the order page

--- frontend/pages/orders.js
+++ frontend/pages/orders.js
@@ -1,9 +1,19 @@
 import Layout from "../components/Layout";
+import Link from "next/link";
 
 const Orders = () => {
-  return <Layout>  <h1 className="text-2xl text-gray-800 font-light">
-  Orders
-</h1></Layout>;
+  return (
+    <Layout>
+      <h1 className="text-2xl text-gray-800 font-light">Orders</h1>
+
+      <Link
+        href="/newOrder"
+        className="bg-blue-800 py-2 px-5 my-3 inline-block text-white rounded text-sm hover:bg-gray-800 uppercase font-bold"
+      >
+        New Order
+      </Link>
+    </Layout>
+  );
 };
 
 export default Orders;
