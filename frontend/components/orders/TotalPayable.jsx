4665fd839f53758228482e9a5e588f2c3611c36f
inmacc96 <inmacaballerocarrero@gmail.com>
2023-01-24 17:53:30 +0100

    Create totalPayable component and add a button to save the order

---
+++ frontend/components/orders/TotalPayable.jsx
@@ -0,0 +1,11 @@
+const TotalPayable = () => {
+  const total = 200;
+  return (
+    <div className="flex items-center mt-5 justify-between bg-white p-3">
+      <h2 className="text-gray-800 text-lg">Total payable:</h2>
+      <p className="text-gray-800 mt-0">$ {total}</p>
+    </div>
+  );
+};
+
+export default TotalPayable;
--- frontend/pages/newOrder.js
+++ frontend/pages/newOrder.js
@@ -2,6 +2,7 @@ import Layout from "../components/Layout";
 import AssignCustomer from "../components/orders/AssignCustomer";
 import AssignProducts from "../components/orders/AssignProducts";
 import OrderSummary from "../components/orders/OrderSummary";
+import TotalPayable from "../components/orders/TotalPayable";
 
 const NewOrder = () => {
   return (
@@ -13,6 +14,14 @@ const NewOrder = () => {
           <AssignCustomer />
           <AssignProducts />
           <OrderSummary />
+          <TotalPayable />
+
+          <button
+            type="button"
+            className={`bg-gray-800 w-full mt-5 p-2 text-white uppercase font-bold hover:bg-gray-900`}
+          >
+            Register Order
+          </button>
         </div>
       </div>
     </Layout>
