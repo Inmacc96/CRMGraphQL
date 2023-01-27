8595c02a1b640d431c0804d0072e4404c81f3105
inmacc96 <inmacaballerocarrero@gmail.com>
2023-01-27 10:58:26 +0100

    Create bestSellers page

---
+++ frontend/pages/bestSellers.js
@@ -0,0 +1,11 @@
+import Layout from "../components/Layout";
+
+const BestSellers = () => {
+  return (
+    <Layout>
+      <h1 className="text-2xl text-gray-800 font-light">Best Sellers</h1>
+    </Layout>
+  );
+};
+
+export default BestSellers;
