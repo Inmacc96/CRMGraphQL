6dc52c9a07d831e58f35b399cfa5a3311cf51655
inmacc96 <inmacaballerocarrero@gmail.com>
2023-01-27 12:46:49 +0100

    - Get the bestCustomers from the database and plot them on the chart - Get the data in real time

---
+++ frontend/pages/bestCustomers.js
@@ -0,0 +1,77 @@
+import { useEffect } from "react";
+import Layout from "../components/Layout";
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+} from "recharts";
+import { gql, useQuery } from "@apollo/client";
+
+const BEST_CUSTOMERS = gql`
+  query getBestCustomers {
+    getBestCustomers {
+      total
+      customer {
+        id
+        name
+        company
+      }
+    }
+  }
+`;
+
+const BestCustomers = () => {
+  const { data, loading, startPolling, stopPolling } = useQuery(BEST_CUSTOMERS);
+
+  useEffect(() => {
+    startPolling(1000);
+    return () => {
+      stopPolling();
+    };
+  }, [startPolling, stopPolling]);
+
+  if (loading) return <p>Loading...</p>;
+
+  const { getBestCustomers } = data;
+
+  // Aplanar getBestCustomers
+  const customersGraph = [];
+
+  getBestCustomers.forEach((customer, index) => {
+    customersGraph[index] = {
+      ...customer.customer[0],
+      total: customer.total,
+    };
+  });
+  return (
+    <Layout>
+      <h1 className="text-2xl text-gray-800 font-light">Best Customers</h1>
+
+      <BarChart
+        className="mt-10"
+        width={600}
+        height={500}
+        data={customersGraph}
+        margin={{
+          top: 5,
+          right: 30,
+          left: 20,
+          bottom: 5,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Bar dataKey="total" fill="#3182ce" />
+      </BarChart>
+    </Layout>
+  );
+};
+
+export default BestCustomers;
