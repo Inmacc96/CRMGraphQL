e559ee2d3ee3bdd15f4990c7d57edea1e0a03ccf
inmacc96 <inmacaballerocarrero@gmail.com>
2023-01-31 19:51:21 +0100

    Improve styles products tablet and new product button

--- frontend/pages/products.js
+++ frontend/pages/products.js
@@ -18,28 +18,30 @@ const Products = () => {
 
       <Link
         href="/newProduct"
-        className="bg-blue-800 px-5 py-2 mt-3 inline-block text-white hover:bg-gray-800 hover:text-gray-200 mb-3 rounded uppercase font-bold text-sm"
+        className="w-full lg:w-auto text-center bg-blue-800 px-5 py-2 mt-3 inline-block text-white hover:bg-gray-800 hover:text-gray-200 mb-3 rounded uppercase font-bold text-sm"
       >
         New Product
       </Link>
 
-      <table className="table-auto shadow-md mt-10 w-full">
-        <thead className="bg-gray-800">
-          <tr className="text-white">
-            <th className="w-1/5 py-2">Name</th>
-            <th className="w-1/5 py-2">Stock</th>
-            <th className="w-1/5 py-2">Price</th>
-            <th className="w-1/5 py-2">Delete</th>
-            <th className="w-1/5 py-2">Edit</th>
-          </tr>
-        </thead>
+      <div className="overflow-x-scroll">
+        <table className="table-auto shadow-md mt-10 w-full">
+          <thead className="bg-gray-800">
+            <tr className="text-white">
+              <th className="w-1/5 py-2">Name</th>
+              <th className="w-1/5 py-2">Stock</th>
+              <th className="w-1/5 py-2">Price</th>
+              <th className="w-1/5 py-2">Delete</th>
+              <th className="w-1/5 py-2">Edit</th>
+            </tr>
+          </thead>
 
-        <tbody className="bg-white">
-          {data.getProducts.map((product) => (
-            <Product key={product.id} product={product} />
-          ))}
-        </tbody>
-      </table>
+          <tbody className="bg-white">
+            {data.getProducts.map((product) => (
+              <Product key={product.id} product={product} />
+            ))}
+          </tbody>
+        </table>
+      </div>
     </Layout>
   );
 };
