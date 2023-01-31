9dc8cee6cf55ce075e9047dafed8a6b7bee34063
inmacc96 <inmacaballerocarrero@gmail.com>
2023-01-31 18:27:52 +0100

    Fix prop referrerPolicy

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -9,7 +9,7 @@ const Document = () => {
           href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
           integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w=="
           crossOrigin="anonymous"
-          referrerpolicy="no-referrer"
+          referrerPolicy="no-referrer"
         />
       </Head>
 
