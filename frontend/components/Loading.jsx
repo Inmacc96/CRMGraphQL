dd2757bfd56af38b6d38f6369d6e1b26f7b341ae
inmacc96 <inmacaballerocarrero@gmail.com>
2023-02-03 19:11:45 +0100

    Fix className prop

--- frontend/components/Loading.jsx
+++ frontend/components/Loading.jsx
@@ -7,7 +7,7 @@ const Loading = () => {
         <div role="status">
           <svg
             aria-hidden="true"
-            class="inline w-10 h-10 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-800"
+            className="inline w-10 h-10 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-800"
             viewBox="0 0 100 101"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
@@ -21,7 +21,7 @@ const Loading = () => {
               fill="currentFill"
             />
           </svg>
-          <span class="sr-only">Loading...</span>
+          <span className="sr-only">Loading...</span>
         </div>
       </div>
     </Layout>
